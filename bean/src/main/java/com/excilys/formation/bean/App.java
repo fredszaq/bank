da69ba75c531663dfdfde3945e5152f3d7fc13a0
Thibaut Lorrain <fredszaq@gmail.com>
2012-03-07 11:55:44 +0100

    this is a test

--- bean/src/main/java/com/excilys/formation/bean/App.java
+++ bean/src/main/java/com/excilys/formation/bean/App.java
@@ -2,12 +2,11 @@ package com.excilys.formation.bean;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		// this is a test
+	}
 }
