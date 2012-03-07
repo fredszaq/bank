e29bee9bda701db65f5dad96679b6c3ee58797c8
Stan Bertrand <sbertrand@excilys.com>
2012-03-07 15:24:08 +0100

    test

---
+++ bean/src/main/java/com/excilys/formation/bean/CopyOfApp.java
@@ -0,0 +1,12 @@
+package com.excilys.formation.bean;
+
+/**
+ * Hello world!
+ * 
+ */
+public class CopyOfApp {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		// this is a test
+	}
+}
