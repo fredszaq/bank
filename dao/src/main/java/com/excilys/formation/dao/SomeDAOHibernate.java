eaf7394f678c27793b0362e611ccc0acf635719c
Luc Mazon <lmazon@excilys.com>
2012-03-08 11:53:12 +0100

    javadoc modified

--- dao/src/main/java/com/excilys/formation/dao/SomeDAOHibernate.java
+++ dao/src/main/java/com/excilys/formation/dao/SomeDAOHibernate.java
@@ -7,41 +7,86 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * A dao class to work with hibernate 4.
+ * 
+ * @author excilys
+ * 
+ */
 @Repository
 public class SomeDAOHibernate implements SomeDAOInterface {
 
+	/**
+	 * The Hibernate SessionFactory.
+	 */
 	@Autowired
 	private SessionFactory sessionFactory;
 
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
+	/**
+	 * set the SessionFactory.
+	 * 
+	 * @param factory
+	 *            the factory to be used
+	 */
+	public final void setSessionFactory(SessionFactory factory) {
+		this.sessionFactory = factory;
 	}
 
+	/**
+	 * get an entity by string.
+	 * 
+	 * @param string
+	 *            the string
+	 * @return the entity
+	 */
 	@Transactional
-	public SomeEntity getEntityByString(String string) {
+	public final SomeEntity getEntityByString(String string) {
 		return (SomeEntity) sessionFactory.getCurrentSession().get(
 				SomeEntity.class, string);
 
 	}
 
+	/**
+	 * find all entities.
+	 * 
+	 * @return the list of all entities
+	 */
 	@Transactional
-	public List<SomeEntity> findAllEntities() {
+	public final List<SomeEntity> findAllEntities() {
 		return sessionFactory.getCurrentSession()
 				.createQuery("from SomeEntity").list();
 	}
 
+	/**
+	 * save an entity.
+	 * 
+	 * @param entity
+	 *            the entity to save
+	 */
 	@Transactional
-	public void saveEntity(SomeEntity entity) {
+	public final void saveEntity(SomeEntity entity) {
 		sessionFactory.getCurrentSession().save(entity);
 	}
 
+	/**
+	 * update an existing entity.
+	 * 
+	 * @param entity
+	 *            the entity to update
+	 */
 	@Transactional
-	public void updateEntity(SomeEntity entity) {
+	public final void updateEntity(SomeEntity entity) {
 		sessionFactory.getCurrentSession().update(entity);
 	}
 
+	/**
+	 * delete an entity.
+	 * 
+	 * @param entity
+	 *            the entity to delete
+	 */
 	@Transactional
-	public void deleteEntity(SomeEntity entity) {
+	public final void deleteEntity(SomeEntity entity) {
 		sessionFactory.getCurrentSession().delete(entity);
 	}
 
