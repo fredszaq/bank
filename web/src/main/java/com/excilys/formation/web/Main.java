72cea7c0a5600632a3c4208f1d74729f43f6abe6
Thibaut Lorrain <fredszaq@gmail.com>
2012-03-08 10:14:14 +0100

    Correction de problèmes checkstyle

--- web/src/main/java/com/excilys/formation/web/Main.java
+++ web/src/main/java/com/excilys/formation/web/Main.java
@@ -1,13 +1,28 @@
 package com.excilys.formation.web;
 
+/**
+ * Main class used for test purposes.
+ *
+ * TODO delete this ;D
+ *
+ * @author excilys
+ *
+ */
 public class Main {
 
 	/**
-	 * @param args
+	 * @param args cli arguments
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * private contructor to prevent the instanciation of an object of this
+	 * type.
+	 */
+	private Main() {
+		// nothing here
+	}
 }
--- web/src/test/java/com/excilys/formation/web/Test.java
+++ web/src/test/java/com/excilys/formation/web/Test.java
@@ -1,5 +1,13 @@
 package com.excilys.formation.web;
 
+/**
+ * A class used for test purposes.
+ *
+ * TODO delete this :D
+ *
+ * @author excilys
+ *
+ */
 public class Test {
 
 }
