843a54dd1d960ad132e3d8bc86b0c9a27da495fb
Thibaut Lorrain <fredszaq@gmail.com>
2012-03-12 17:22:35 +0100

    augmentation de la couverture des tests

--- dao/src/test/java/com/excilys/formation/dao/SimpleTest.java
+++ dao/src/test/java/com/excilys/formation/dao/SimpleTest.java
@@ -3,6 +3,7 @@ package com.excilys.formation.dao;
 import static org.fest.assertions.Assertions.assertThat;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -30,6 +31,44 @@ public class SimpleTest {
 
 	@Autowired
 	private SomeDAOInterface someDAOHibernate;
+
+	/**
+	 * a test without any asserts
+	 */
+	@Test
+	public final void anotherFuckingTest() {
+
+		SomeEntity entity = new SomeEntity();
+		entity.setSomeDate(new GregorianCalendar(2010, 1, 2).getTime());
+		entity.setSomeInteger(3);
+		entity.setSomeString("mwahaha");
+
+		SomeEntity entity2 = new SomeEntity();
+		entity2.setSomeDate(new GregorianCalendar(2012, 1, 2).getTime());
+		entity2.setSomeInteger(2);
+		entity2.setSomeString("hohoho");
+
+		this.someDAOHibernate.saveEntity(entity);
+		this.someDAOHibernate.saveEntity(entity2);
+
+		for (SomeEntity someEntity : this.someDAOHibernate.findAllEntities()) {
+			System.out.println(someEntity);
+		}
+
+		entity.setSomeInteger(5);
+		entity2.setSomeDate(new GregorianCalendar(2011, 1, 2).getTime());
+
+		this.someDAOHibernate.updateEntity(entity);
+		this.someDAOHibernate.updateEntity(entity2);
+
+		for (SomeEntity someEntity : this.someDAOHibernate.findAllEntities()) {
+			System.out.println(someEntity);
+		}
+
+		this.someDAOHibernate.deleteEntity(entity);
+		this.someDAOHibernate.deleteEntity(entity2);
+
+	}
 
 	/**
 	 * A cobertura test.
