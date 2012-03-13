f9ec5706f4465066ec33e391314205af5ed00c0b
Thibaut Lorrain <fredszaq@gmail.com>
2012-03-13 15:30:31 +0100

    test augmentant la couverture

--- bean/src/test/java/com/excilys/formation/bank/bean/SimpleTest.java
+++ bean/src/test/java/com/excilys/formation/bank/bean/SimpleTest.java
@@ -1,5 +1,9 @@
 package com.excilys.formation.bank.bean;
 
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.Date;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,6 +19,21 @@ public class SimpleTest {
 	@Test
 	public final void oneEqualsOne() {
 		Assert.assertEquals(1, 1);
+	}
+
+	/**
+	 * Another very rigorous test.
+	 */
+	@Test
+	public final void testSomeEntity() {
+		SomeEntity entity = new SomeEntity();
+		Date date = new Date();
+		entity.setSomeDate(date);
+		assertThat(entity.getSomeDate()).isEqualTo(date);
+		entity.setSomeInteger(1);
+		assertThat(entity.getSomeInteger()).isEqualTo(1);
+		entity.setSomeString("hello");
+		assertThat(entity.getSomeString()).isEqualTo("hello");
 	}
 
 }
