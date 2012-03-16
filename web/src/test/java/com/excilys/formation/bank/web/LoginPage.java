ee6f8104b7d318297a8183c17e5ec080794046b4
Thibaut Lorrain <fredszaq@gmail.com>
2012-03-16 10:59:04 +0100

    tests selenium

--- web/src/test/java/com/excilys/formation/bank/web/LoginPage.java
+++ web/src/test/java/com/excilys/formation/bank/web/LoginPage.java
@@ -1,7 +1,20 @@
 package com.excilys.formation.bank.web;
 
+import static org.fest.assertions.Assertions.assertThat;
+
 import org.fluentlenium.core.FluentPage;
 
 public class LoginPage extends FluentPage {
 
+	@Override
+	public String getUrl() {
+		return TestConstants.BASE_URL + "login.html";
+	}
+
+	@Override
+	public void isAt() {
+		assertThat(find("#j_username")).hasSize(1);
+		assertThat(find("#j_password")).hasSize(1);
+		assertThat(find("#_spring_security_remember_me")).hasSize(1);
+	}
 }
