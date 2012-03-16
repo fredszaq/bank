b671cdcd9958a45010cb4bee93b58dfa79fa0862
Thibaut Lorrain <fredszaq@gmail.com>
2012-03-16 11:18:09 +0100

    modification pom.xml pour que les tests d'integration passent sur le jenkins

--- web/src/test/java/com/excilys/formation/bank/web/TestConstants.java
+++ web/src/test/java/com/excilys/formation/bank/web/TestConstants.java
@@ -1,11 +1,17 @@
 package com.excilys.formation.bank.web;
 
+/**
+ * This interfaces defines constants used in the tests.
+ * 
+ * @author excilys
+ * 
+ */
 public interface TestConstants {
 	/**
-	 * The base URL where the application is deployed, you can change this if
+	 * The base URL where the application is deployed. You can change this if
 	 * you have deployed the application elsewhere, but please do not commit the
 	 * changes as this is the URL used on the Jenkins server (and the one used
 	 * if you run Maven)
 	 */
-	public static String BASE_URL = "http://localhost:9090/bankroot/";
+	String BASE_URL = "http://localhost:9090/bankroot/";
 }
