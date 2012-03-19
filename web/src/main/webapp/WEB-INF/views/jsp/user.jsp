73bf0ddeaae2780b1671836dbed1486e1bc7b941
Thibaut Lorrain <fredszaq@gmail.com>
2012-03-19 14:39:27 +0100

    tests plus poussés de la securité

--- web/src/main/webapp/WEB-INF/views/jsp/user.jsp
+++ web/src/main/webapp/WEB-INF/views/jsp/user.jsp
@@ -4,5 +4,5 @@
 	xmlns:c="http://java.sun.com/jsp/jstl/core"
 	xmlns:fmt="http://java.sun.com/jsp/jstl/fmt">
 	<fmt:setBundle basename="localization.Messages" />
-	This is a secured page !
+	This is a secured page ! Only logged users can view this ! 
 </jsp:root>
---
+++ web/src/test/java/com/excilys/formation/bank/web/AdminPage.java
@@ -0,0 +1,33 @@
+package com.excilys.formation.bank.web;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.fluentlenium.core.FluentPage;
+
+/**
+ * This class uses the PageObject pattern and describes the main admin page of
+ * the application.
+ * 
+ * @author excilys
+ * 
+ */
+public class AdminPage extends FluentPage {
+
+	@Override
+	public final String getUrl() {
+		return TestConstants.BASE_URL + "secure/admin/admin.html";
+	}
+
+	@Override
+	public final void isAt() {
+		assertThat(pageSource()).contains("Only admins can view this !");
+	}
+
+	/**
+	 * Checks with assertions that the page is showing errors.
+	 */
+	public final void isShowingErrors() {
+		assertThat(find(".error")).isNotEmpty();
+	}
+
+}
