6a3da8dbbd2eb2cbdd7e94bb50585155fd72565a
Luc Mazon <lmazon@excilys.com>
2012-03-19 14:38:05 +0100

    ajout de deux utilisateurs authentifiés

---
+++ src/etc/env/sql/migration/V1_h__add_authenticated_user.sql
@@ -0,0 +1,14 @@
+-- Ajout de plusieurs utilisateurs authentifiés
+
+-- Ajout à la table users
+INSERT INTO
+	users (login, password, lastname, firstname)
+VALUES
+	('robert', 'abec4c69a40a5d29fddf759532294c2f003384c9f6396d8ca54e487f604b4af8c9f0887d5ec3b782', 'Dupont', 'Robert'),
+	('jacky', '733ff2a136ad7ce76d0025fb645adddc520a79a7ad9087f41a18fddd82a2701ee22b4d703cbc677e', 'Moutmout', 'Jacky');
+
+INSERT INTO
+	users_authorities (login, authority_id)
+VALUES
+	('robert', 1),
+	('jacky', 1);
