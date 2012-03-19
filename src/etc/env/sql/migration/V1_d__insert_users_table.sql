d2194ddcdc49bac483b00972cfbb317d882e9fe7
Luc Mazon <lmazon@excilys.com>
2012-03-19 11:17:36 +0100

    mots de passe administrateurs salés et sha-256és

--- src/etc/env/sql/migration/V1_d__insert_users_table.sql
+++ src/etc/env/sql/migration/V1_d__insert_users_table.sql
@@ -3,6 +3,6 @@
 INSERT INTO
 	users (login, password, lastname, firstname)
 VALUES
-	('luc', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Mazon', 'Luc'),
-	('fredszaq', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Lorrain', 'Thibaut'),
-	('stan', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Bertrand', 'Stan');
+	('luc', 'a4f11d39390b85893cb8ef3faa7402f997060431981a463e8c270c319d346c1cccfcc2955bea30a4', 'Mazon', 'Luc'),
+	('fredszaq', '12fa17de82543e7b6834511c0881a01f749d624c5b8f325766a132c508bdffb55073de3689457fd5', 'Lorrain', 'Thibaut'),
+	('stan', '835ab94f6450c735b1ca1cbdf486127dd9c422f7002a0966af016458371ac75173fa37cc9aa69781', 'Bertrand', 'Stan');
