7967931e4121e0583556b6e4af6fe735f6b765d1
Thibaut Lorrain <fredszaq@gmail.com>
2012-03-22 10:39:06 +0100

    page de détail de compte

---
+++ service/src/test/java/com/excilys/formation/bank/service/TestUserService.java
@@ -0,0 +1,42 @@
+package com.excilys.formation.bank.service;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import com.excilys.ebi.spring.dbunit.test.DataSet;
+import com.excilys.ebi.spring.dbunit.test.DataSetTestExecutionListener;
+import com.excilys.formation.bank.bean.Compte;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath*:context/applicationContext*.xml",
+		"classpath*:contextTest/applicationContext*.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+		DataSetTestExecutionListener.class })
+@DataSet("/dataSetUserService.xml")
+public class TestUserService {
+	@Autowired
+	private UserService userService;
+
+	@Test
+	public void testGetCompteByUsernameAndAccountIdWhenTheUserDoesNotOwnsTheCompte() {
+		Compte compte = userService.getCompteByUsernameAndAccountId(
+				"testUser2", "compte1user1");
+		assertThat(compte).isNull();
+	}
+
+	@Test
+	public void testGetCompteByUsernameAndAccountIdWhenTheUserOwnsTheCompte() {
+		Compte compte = userService.getCompteByUsernameAndAccountId(
+				"testUser2", "compte1user2");
+		assertThat(compte.getCompteId()).isEqualTo("compte1user2");
+		assertThat(compte.getCompteType()).isEqualTo(Compte.CompteType.COURANT);
+		assertThat(compte.getSolde()).isEqualTo(3.14);
+	}
+}
