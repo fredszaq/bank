824d43432788077b72870294a07b70dc8e17d83f
Thibaut Lorrain <fredszaq@gmail.com>
2012-03-23 15:05:45 +0100

    ajout methode get by username and Compte id a compteDao

--- dao/src/main/java/com/excilys/formation/bank/dao/CompteDAO.java
+++ dao/src/main/java/com/excilys/formation/bank/dao/CompteDAO.java
@@ -4,5 +4,24 @@ import com.excilys.formation.bank.bean.Compte;
 
 public interface CompteDAO {
 
+	/**
+	 * retrieve a Compte by id.
+	 * 
+	 * @param id
+	 *            the compte id
+	 * @return the compte
+	 */
 	Compte loadCompteById(String id);
+
+	/**
+	 * retrieve a Compte by id only if it belongs to the given user.
+	 * 
+	 * @param username
+	 *            the username
+	 * @param compteId
+	 *            the compte id
+	 * @return the compte (or null if the COmpte doesn't exists or isn't owned
+	 *         by the user
+	 */
+	Compte loadCompteByUsernameAndCompteId(String username, String compteId);
 }
---
+++ dao/src/test/java/com/excilys/formation/bank/dao/CompteDaoTest.java
@@ -0,0 +1,50 @@
+package com.excilys.formation.bank.dao;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.excilys.ebi.spring.dbunit.test.DataSet;
+import com.excilys.ebi.spring.dbunit.test.DataSetTestExecutionListener;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+		"classpath*:context/applicationContext*.xml",
+		"classpath*:contextTest/applicationContext*.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+		DataSetTestExecutionListener.class })
+@DataSet("/datasets/dataSetCompteDao.xml")
+@Transactional
+@TransactionConfiguration
+public class CompteDaoTest extends
+		AbstractTransactionalJUnit4SpringContextTests {
+
+	@Autowired
+	private CompteDAO compteDAO;
+
+	@Test
+	public final void loadCompteByUsernameAndCompteId() {
+		assertThat(
+				compteDAO.loadCompteByUsernameAndCompteId("testUser1",
+						"compte1user1")).isEqualTo(
+				compteDAO.loadCompteById("compte1user1"));
+	}
+
+	@Test
+	public final void loadCompteByUsernameAndCompteIdWhenTheUserDoesNotOwnTheCompte() {
+		assertThat(compteDAO.loadCompteById("compte1user2")).isNotNull();
+		assertThat(
+				compteDAO.loadCompteByUsernameAndCompteId("testUser1",
+						"compte1user2")).isNull();
+	}
+
+}
