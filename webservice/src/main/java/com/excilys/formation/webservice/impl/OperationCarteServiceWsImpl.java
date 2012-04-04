1308dedc4f4be0657675ddf5d35dcbf7b7a3a8e1
Stan Bertrand <sbertrand@excilys.com>
2012-04-04 15:09:16 +0200

    modification des classes du webservice qui appel la couche service

--- webservice/src/main/java/com/excilys/formation/webservice/impl/OperationCarteServiceWsImpl.java
+++ webservice/src/main/java/com/excilys/formation/webservice/impl/OperationCarteServiceWsImpl.java
@@ -1,6 +1,9 @@
 package com.excilys.formation.webservice.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.excilys.formation.bank.bean.Transaction;
+import com.excilys.formation.bank.service.OperationCarteService;
 import com.excilys.formation.webservice.OperationCarteServiceWs;
 
 /**
@@ -11,12 +14,16 @@ import com.excilys.formation.webservice.OperationCarteServiceWs;
  */
 public class OperationCarteServiceWsImpl implements OperationCarteServiceWs {
 
+	@Autowired
+	private OperationCarteService operationCarteService;
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public final Transaction createOperationCarte(String login,
 			String compteDebiteurId, long montant, String libelle) {
-		return null;
+		return operationCarteService.createOperationCarte(login,
+				compteDebiteurId, montant, libelle);
 	}
 }
