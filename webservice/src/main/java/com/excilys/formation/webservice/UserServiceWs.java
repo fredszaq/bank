b963f217b8a3a134c53b292353ca1795f3ede240
Stan Bertrand <sbertrand@excilys.com>
2012-04-05 12:14:08 +0200

    refactoring webservice

--- webservice/src/main/java/com/excilys/formation/webservice/UserServiceWs.java
+++ webservice/src/main/java/com/excilys/formation/webservice/UserServiceWs.java
@@ -1,6 +1,5 @@
 package com.excilys.formation.webservice;
 
-import java.util.List;
 import java.util.Set;
 
 import javax.jws.WebMethod;
@@ -10,7 +9,6 @@ import javax.jws.WebService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.excilys.formation.bank.bean.Compte;
-import com.excilys.formation.bank.bean.Operation;
 import com.excilys.formation.bank.bean.User;
 
 /**
@@ -22,20 +20,6 @@ import com.excilys.formation.bank.bean.User;
 
 @WebService
 public interface UserServiceWs {
-
-	/**
-	 * Get a specific account of a user.
-	 * 
-	 * @param login
-	 *            the login of the user
-	 * @param id
-	 *            the id of the account
-	 * @return the account (null if not found)
-	 */
-	@WebMethod
-	Compte getCompteByUsernameAndAccountId(
-			@WebParam(name = "login") String login,
-			@WebParam(name = "id") String id);
 
 	/**
 	 * Get the compte by username.
@@ -53,44 +37,5 @@ public interface UserServiceWs {
 	 */
 	@WebMethod
 	User loadUserByUsername(@WebParam(name = "login") String login);
-
-	/**
-	 * Obtention des opérations carte pour un compte donné.
-	 * 
-	 * @param compteId
-	 *            : the compteId
-	 * @param month
-	 *            : month
-	 * @return Liste d'opération
-	 */
-	@WebMethod
-	List<Operation> getOperationsCarteByCompteId(
-			@WebParam(name = "compteId") String compteId,
-			@WebParam(name = "month") Integer month);
-
-	/**
-	 * Obtention des opérations non carte pour un compte donné.
-	 * 
-	 * @param compteId
-	 *            : the compteId
-	 * @param month
-	 *            : month
-	 * @return Liste d'opération
-	 */
-	@WebMethod
-	List<Operation> getOperationsNonCarteByCompteId(
-			@WebParam(name = "compteId") String compteId,
-			@WebParam(name = "month") Integer month);
-
-	/**
-	 * Obtention du total des opérations cartes pour un compte donné.
-	 * 
-	 * @param compteId
-	 *            : the compteId
-	 * @return total
-	 */
-	@WebMethod
-	double getTotalOperationsCarteByCompteId(
-			@WebParam(name = "compteId") String compteId);
 
 }
